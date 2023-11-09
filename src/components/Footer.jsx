3ba87250b740f636c55cc9067bda3651ba1b44eb
Abhishek Kumar <abhishekkumar958830@gmail.com>
2023-11-09 21:43:07 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,18 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <h1>TechyStar</h1>
+        <h1>CodeSecurity</h1>
         <p>@all right reserved</p>
       </div>
 
       <div>
         <h5>Follow Us</h5>
         <div>
-          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
-            Youtube
-          </a>
-          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+          
+          <a href="https://instagram.com/abhishek.coder" target={"blank"}>
             Instagram
           </a>
-          <a href="https://github.com/meabhisingh" target={"blank"}>
+          <a href="https://github.com/abhishek-kumar-91" target={"blank"}>
             Github
           </a>
         </div>
